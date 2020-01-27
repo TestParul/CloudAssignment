db3fdd7ced95d21ffaf8dc4bc77df350622678f3
pjumbad <Parmeshwar.jumbad@carrierbuilder.com>
2020-01-27 12:07:44 +0530

    initial commit

---
+++ EmployeeDetails/EmployeeDetails/EmployeeDetails/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeDetails.Models;
+using Newtonsoft.Json;
+
+namespace EmployeeDetails.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Index()
+        {
+            WebClient webClient = new WebClient();
+            string json = webClient.DownloadString(@"https://cloud-basics-assignment.s3.ap-south-1.amazonaws.com/employee_details.json");
+            //Employee employees = JsonConvert.DeserializeObject<Employee>(json);
+            List<Employee> employees = JsonConvert.DeserializeObject<List<Employee>>(json);
+            return View(employees);
+        }
+    }
+}
---
+++ EmployeeDetails/EmployeeDetails/EmployeeDetails/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeDetails.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string CreatedDate { get; set; }
+        public string UpdatedDate { get; set; }
+    }
+}
